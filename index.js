e9dcd76a8c75740c7ace023ddfe02ab2a5e402a2
Jason Yu <me@ycmjason.com>
2017-12-12 07:36:26 +0000

    changed title
    refactored a bit
    
    edited pre commit hook to build js too
    
    edited pre commit hook to build js too
    
    added app SECRET as a default for salt to make hash consistant
    
    compiled
    
    added generation of image datauri
    
    added background songs
    
    added images for kyt
    
    added spinner
    
    added compiled
    
    testing hook
    
    hook working
    
    added image controller which serves images
    
    removed spinner image
    
    added koda and sam
    
    added more pms
    
    added more pms and loging upon visiting pm
    
    added arthur
    
    removed all private messages

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const pms = require('./lib/store/pms');
+
+pms.forEach(({hash, name}) => console.log(`${name}: http://xmas.ycmjason.com/${hash}`));
+console.log();
+
 require('./lib/app').listen(process.env.PORT || 3000);
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,12 @@
 import "babel-polyfill";
 
+const removeClass = (elem, cls) => elem.className = elem.className.split(/\s+/g).filter(c => c !== cls).join(' ');
+
+const addClass = (elem, ...clses) => elem.className = [
+  ...elem.className.split(/\s+/g),
+  ...clses,
+].join(' ');
+
 function setGreetings(msg){
   document.querySelector('.card_greetings').innerHTML = msg;
 }
@@ -8,15 +15,31 @@ function setBody(msg){
   document.querySelector('#body').innerHTML = msg;
 }
 
+function showCard(){
+  const card = document.querySelector('.card');
+  addClass(card, 'slideInLeft');
+  removeClass(card, 'hide');
+}
+
+function finishLoading(){
+  const loading = document.querySelector('.loading');
+  addClass(loading, 'slideOutRight');
+  loading.remove();
+}
+
 const fetchPM =  async (hash) => {
 	return await fetch(`/pm/${hash}`, { method: 'GET' }).then(res => res.json());
 };
 
 const main = async () => {
   const hash = window.location.href.substring(window.location.origin.length + 1);
-  const pm = await fetchPM(hash);
-  setGreetings(`Dear ${pm.name},`);
-  setBody(pm.body);
+  if(hash) {
+    const pm = await fetchPM(hash);
+    setGreetings(`Dear ${pm.name},`);
+    setBody(pm.body);
+  }
+  finishLoading();
+  showCard();
 };
 
 main().then();
